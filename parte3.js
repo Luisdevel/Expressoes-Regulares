b1637c12bcdd8c2f786e41fe71d3d9fc9c8515b3
Luis Henrique <luishenrique.mf01@gmail.com>
2020-11-04 03:19:03 -0300

    Quantificadores

--- parte3.js
+++ parte3.js
@@ -1,11 +1,24 @@
-const { texto } = require('./base');
+const { texto, arquivos } = require('./base');
 
-// * (opcionais) 0 ou n
-// + (obrigatório) 1 ou n
+// * (opcionais) 0 ou n {0,}
+// + (obrigatório) 1 ou n {1,}
 // ? (opcionais) 0 ou 1
+// \ Caractere de escape
+// {n,m} - min e max
+// {10,} no min 10
+// {,10} de 0 a 10
+// {5,10} de 5 a 10
 
-console.log(texto);
+// console.log(texto);
+// // const regExpt1 = /Jo+ão+/gi;
+// console.log(texto.match(regExpt1));
 
-// const regExpt1 = /Jo+ão+/gi;
+const regExp2 = /\.((jp|JP)(e|E)*(g|G))/g
 
-console.log(texto.match(regExpt1));
+for (const arquivo of arquivos) {
+    const valido = arquivo.match(regExp2);
+
+    // if(!valido) continue;
+
+    console.log(arquivo, valido);
+}
