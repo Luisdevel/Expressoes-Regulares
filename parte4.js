9626bac8a0fe9169eecedaee43654a0e27a7013f
Luis Henrique <luishenrique.mf01@gmail.com>
2020-11-04 15:50:07 -0300

    Greedy e Non-greedy (Iazy)

--- parte4.js
+++ parte4.js
@@ -1,4 +1,4 @@
 const { html } = require('./base');
 
 console.log(html.match(/<.+>.+<\/.+>/g)); // greedy pega tudo o que tem dentro
-console.log(html.match(/<.+?>.+?<\/.+?>/g)); // non-greedy
+console.log(html.match(/<.+?>.+?<\/.+?>/g)); // non-greedy 
