9857f3f3683a0b5ed6b641ec0d995a3724c9e02c
Luis Henrique <luishenrique.mf01@gmail.com>
2020-11-04 20:20:44 -0300

    Começa com e Termina com

---
+++ parte8.js
@@ -0,0 +1,12 @@
+const { cpfs2 } = require('./base');
+
+// ^ -> Cmeça com
+// $ -> Termina com
+// [^] -> Negação
+// m - multiline
+
+const cpf = '254.224.877-45';
+const cpfRegExp = /^(\d{3}\.){2}\d{3}\-\d{2}$/gm;
+
+console.log(cpfs2);
+console.log(cpfs2.match(cpfRegExp));
