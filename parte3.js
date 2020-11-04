dd1e4e76906968d623935557cba131e07bd8a05d
Luis Henrique <luishenrique.mf01@gmail.com>
2020-11-04 02:59:46 -0300

    Quantificadores

---
+++ parte3.js
@@ -0,0 +1,11 @@
+const { texto } = require('./base');
+
+// * (opcionais) 0 ou n
+// + (obrigatório) 1 ou n
+// ? (opcionais) 0 ou 1
+
+console.log(texto);
+
+// const regExpt1 = /Jo+ão+/gi;
+
+console.log(texto.match(regExpt1));
